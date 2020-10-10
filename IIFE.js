1070fe64172d02375bd47f532eb5e65c0824f394
Ishaan Jain <ishaan160799@gmail.com>
2020-10-10 20:11:26 +0530

    IIFE: scope operations

--- IIFE.js
+++ IIFE.js
@@ -1,5 +1,18 @@
 //IIFE = Immediately Invoked Function Expression
 
 (function (){
-    console.log("hello")
+    var a =10
+    var b = a/5
+
+    console.log(a + b)
 })()
+
+
+//Scope operations for IIFE
+if(true){
+    (function (){
+        var a = 10
+    })()
+}
+
+console.log(a)
