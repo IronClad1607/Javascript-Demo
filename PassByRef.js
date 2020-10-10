c2158ef5c8badd9b4150f903d805103fc520463a
Ishaan Jain <ishaan160799@gmail.com>
2020-10-10 19:35:49 +0530

    PassByRef:file started

---
+++ PassByRef.js
@@ -0,0 +1,11 @@
+var quidditchWinner = "Harry Potter"
+
+function changeWinner(winner){
+    winner = "Draco Malfoy"
+}
+
+console.log("The winner was " + quidditchWinner)
+
+changeWinner(quidditchWinner)
+
+console.log("Now the winner is " + quidditchWinner)
