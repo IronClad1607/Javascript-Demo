8ef1fe2fe3d4a0b386a41bae6c8913eaef8c4688
Ishaan Jain <ishaan160799@gmail.com>
2020-10-10 13:43:24 +0530

    json object added

--- LetConstVar.js
+++ LetConstVar.js
@@ -13,5 +13,23 @@ b++
 console.log(b);
 
 const c = 30
-c++
+// c++
 console.log(c)
+
+/**
+ * JSON Object
+ */
+
+const obj = {
+     k1:'asd',
+     k2:10,
+     k3:false
+    }
+
+obj.k4 = 123
+
+console.log(obj)
+console.log(obj.k4)
+
+delete obj.k1
+console.log(obj.k1)
