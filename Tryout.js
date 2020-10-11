00c6ce7e61776985d4e427c2a249146ad78c52b7
Ishaan Jain <ishaan160799@gmail.com>
2020-10-11 15:40:24 +0530

    tryout:clearInterval implemented

--- Tryout.js
+++ Tryout.js
@@ -1,11 +1,18 @@
+var intervalID
+var runCount = 0
+
 function sayHello(){
+    runCount++
+    if(runCount>5){
+        clearInterval(intervalID)
+    }
     console.log("Hello !")
 }
 
 console.log("The waits starts ...")
 
-setTimeout(sayHello,2000)
-setTimeout(sayHello,2000)
+// setTimeout(sayHello,2000)
+// setTimeout(sayHello,2000)
 
 
-setInterval(sayHello,1000)
+intervalID =  setInterval(sayHello,1000)
