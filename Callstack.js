a71e5ad5b3a53307ef6e97282c418c34745e5640
Ishaan Jain <ishaan160799@gmail.com>
2020-10-11 15:24:14 +0530

    Callstack: prepared

---
+++ Callstack.js
@@ -0,0 +1,25 @@
+function ceo(){
+    console.log("Lets call CTO")
+    cto()
+}
+
+function cto(){
+    console.log("Lets call VP Engg")
+    vpEng()
+}
+
+function vpEng(){
+    console.log("Lets call Tech lead")
+    techLead()
+}
+
+function techLead(){
+    console.log("Lets call developer")
+    developer()
+}
+
+function developer(){
+    console.log("Lets develop")
+}
+
+ceo()
